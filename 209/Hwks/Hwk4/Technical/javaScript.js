40cad894cdb2642e3409e6511a3e71fc0e6461b5
Katherine Vogt <kvogt@smith.edu>
2025-02-24 22:26:08 -0500

    2/3s of hwk 4 done

---
+++ 209/Hwks/Hwk4/Technical/javaScript.js
@@ -0,0 +1,41 @@
+   
+function moveRed()
+{   
+    var redSquare = document.getElementById("redSq");   
+    var redPos = 0;
+    var stepRedId = setInterval(stepRed, 10);
+    // Get speed from dropdown
+    var speed = parseInt(document.getElementById("redSpeed").value);
+
+    function stepRed() {
+        if (redPos == 350) {
+            clearInterval(stepRedId);
+        } else {
+            redPos+=(35-speed); 
+            redSquare.style.top = redPos + 'px'; 
+            redSquare.style.left = redPos + 'px';
+        }
+    }
+}
+
+function moveBlue()
+{   
+    var blueSquare = document.getElementById("blueSq");   
+    var bluePos = 350;
+    var stepBlueId = setInterval(stepBlue, 10);
+    // Get speed from dropdown
+    var speed = parseInt(document.getElementById("blueSpeed").value);
+
+    function stepBlue() {
+        if (bluePos == 0) {
+            clearInterval(stepBlueId);
+        }
+         else {
+            bluePos -=(35-speed); 
+            blueSquare.style.top = bluePos + 'px'; 
+            blueSquare.style.left = bluePos + 'px';
+        }
+    }
+}
+
+
---
+++ 209/Weeks/Week4/Animation/js/animation.js
@@ -0,0 +1,36 @@
+   
+function moveRed()
+{   
+    var redSquare = document.getElementById("redSq");   
+    var redPos = 0;
+    var stepRedId = setInterval(stepRed, 10);
+
+    function stepRed() {
+        if (redPos == 350) {
+            clearInterval(stepRedId);
+        } else {
+            redPos++; 
+            redSquare.style.top = redPos + 'px'; 
+            redSquare.style.left = redPos + 'px';
+        }
+    }
+}
+
+function moveBlue()
+{   
+    var blueSquare = document.getElementById("blueSq");   
+    var bluePos = 350;
+    var stepBlueId = setInterval(stepBlue, 10);
+
+    function stepBlue() {
+        if (bluePos == 0) {
+            clearInterval(stepBlueId);
+        } else {
+            bluePos--; 
+            blueSquare.style.top = bluePos + 'px'; 
+            blueSquare.style.left = bluePos + 'px';
+        }
+    }
+}
+
+
