c5f0c21de832608e91a25934dd33717c9c93fce2
Katherine Vogt <kvogt@smith.edu>
2025-03-10 21:53:58 -0400

    more homework edits

--- 209/Hwks/Hwk3/Creative/js/creative.js
+++ 209/Hwks/Hwk3/Creative/js/creative.js
@@ -1,10 +1,10 @@
 function switchTheme () {
     var stylesheet = document.getElementById('stylesheet');
 
-    if(stylesheet.getAttribute('href')=='style2.css') {
-        stylesheet.setAttribute('href', 'style1.css');
+    if(stylesheet.getAttribute('href')=='./css/style2.css') {
+        stylesheet.setAttribute('href', './css/style1.css');
     } else {
-        stylesheet.setAttribute('href', 'style2.css');
+        stylesheet.setAttribute('href', './css/style2.css');
     }
 }
 
