d6d8ce0f1909528ea6907883a228721a58a9d2cf
Katherine Vogt <kvogt@smith.edu>
2025-04-08 21:37:57 -0400

    themes
    working on theme switching

--- 209/Hwks/Hwk7n8/Technical/Level4.html.php
+++ 209/Hwks/Hwk7n8/Technical/Level4.html.php
@@ -59,7 +59,7 @@
         <h1>・┆✦ʚ♡ɞ✦┆・Level4 (Part 2)・┆✦ʚ♡ɞ✦┆・</h1>
         <!--Button to change CSS!-->
         <h2>༺♥༻❤༺♥༻</h2>
-        <button class="center" onclick="switchTheme();">‧❤₊˚♡ Switch theme ♡˚₊❤‧</button>
+        <a href="?switch_theme=true"><button>‧❤₊˚♡ Switch theme ♡˚₊❤‧</button></a>
    
 
         <h3>・┆✦ʚ♡ɞ✦┆・・┆✦ʚ♡ɞ✦┆・</h3>
@@ -69,96 +69,41 @@
     <body>
           <p>Image Path: <?php echo $imagefolder . '/' . $currentImage; ?></p> 
 
-        
+        <h2>✧༺♥༻✧Sorry I am not sure why im having such a hard time w/ PHP!✧༺♥༻✧</h2>
 
       <h2>Switch Theme:</h2>
       <a href="?theme=light-theme.css">Light</a> | <a href="?theme=dark-theme.css">Dark</a>
 
         <h2>╔═══════════════════════════☆♡☆═════════════════════════╗</h2>
         <div class="slideshow-container">
-            <div class="mySlides fade">
-              <div class="numbertext">1 / 15</div>
-              <img src="../images/0E6632B9-D1ED-4548-858B-9370732250FA.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-          
-            <div class="mySlides fade">
-              <div class="numbertext">2 / 15</div>
-              <img src="../images/988C9E48-AC0F-4990-9B34-97BEA25093CA.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-          
-            <div class="mySlides fade">
-              <div class="numbertext">3 / 15</div>
-              <img src="../images/IMG_0006.JPG" style="width:100%">
-              <div class="text"></div>
-            </div>
+        <?php
+          $images = [
+              "../images/0E6632B9-D1ED-4548-858B-9370732250FA.jpeg",
+              "../images/988C9E48-AC0F-4990-9B34-97BEA25093CA.jpeg",
+              "../images/IMG_0006.JPG",
+              "../images/IMG_0088.jpeg",
+              "../images/IMG_0302.jpeg",
+              "../images/IMG_0388.jpeg",
+              "../images/IMG_2309.jpeg",
+              "../images/IMG_4113.jpeg",
+              "../images/IMG_4642.jpeg",
+              "../images/IMG_6502.jpeg",
+              "../images/IMG_7235.JPG",
+              "../images/IMG_7354.jpeg",
+              "../images/IMG_8636.jpeg",
+              "../images/IMG_8897.jpeg"
+          ];
 
-            <div class="mySlides fade">
-              <div class="numbertext">4 / 15</div>
-              <img src="../images/IMG_0088.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">5 / 15</div>
-              <img src="../images/IMG_0302.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">6 / 15</div>
-              <img src="../images/IMG_0388.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">8 / 15</div>
-              <img src="../images/IMG_2309.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">9 / 15</div>
-              <img src="../images/IMG_4113.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">10 / 15</div>
-              <img src="../images/IMG_4642.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">11 / 15</div>
-              <img src="../images/IMG_6502.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">12 / 15</div>
-              <img src="../images/IMG_7235.JPG" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">13 / 15</div>
-              <img src="../images/IMG_7354.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">14 / 15</div>
-              <img src="../images/IMG_8636.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">15 / 15</div>
-              <img src="../images/IMG_8897.jpeg" style="width:100%">
-              <div class="text"></div>
-            </div>
+          $index = 1;
+          foreach ($images as $image) {
+              echo "<div class='mySlides fade'>";
+              echo "<div class='numbertext'>$index / " . count($images) . "</div>";
+              echo "<img src='$image' style='width:100%'>";
+              echo "<div class='text'></div>";
+              echo "</div>";
+              $index++;
+          }
+        ?>
           
             <!-- Next and previous buttons -->
             <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -174,16 +119,7 @@
           </div>
           <h2>╚══════════════════════════☆♡☆═════════════════════════╝</h2>
     
-        
-          
-           
-        
-
-          <h2>✧༺♥༻✧Sorry I am not sure why im havingsuch a hard time w/ PHP!✧༺♥༻✧</h2>
-          <h2>❤‧̍̊·̊‧̥°̩̥˚̩̩̥͙°̩̥‧̥·̊‧̍̊❤ ♡ ❤°̩̥˚̩̩̥͙°̩̥ ·͙*̩̩͙˚̩̥̩̥*̩̩̥͙·̩̩̥͙*̩̩̥͙˚̩̥̩̥*̩̩͙‧͙ °̩̥˚̩̩̥͙°̩̥❤ ♡ ❤‧̍̊·̊‧̥°̩̥˚̩̩̥͙°̩̥‧̥·̊‧̍̊❤</h2>
-
-
-
+       
 
     <!--javascript link-->
     <script src="./js/slideshow.js"></script>
